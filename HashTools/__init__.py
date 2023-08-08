e59672ea10bcc104738643372d15c309385fa2c1
viensea1106 <viensea1106@gmail.com>
2023-08-08 23:27:30 +0700

    init HashTools

---
+++ HashTools/__init__.py
@@ -0,0 +1,10 @@
+from .sha1   import SHA1
+from .sha224 import SHA224
+from .sha256 import SHA256
+from .sha384 import SHA384
+from .sha512 import SHA512
+
+__version__ = "0.0.1"
+__all__     = (
+    "sha1", "sha224", "sha256", "sha384", "sha512", "utils"
+) 
