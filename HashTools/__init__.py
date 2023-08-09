fdc92d23687a48c38432133adf58937a504ce98f
viensea1106 <viensea1106@gmail.com>
2023-08-09 20:56:43 +0700

    handling exceptions

--- HashTools/__init__.py
+++ HashTools/__init__.py
@@ -8,18 +8,36 @@ from .sha512 import SHA512
 from typing import Union
 
 __version__ = "0.0.1"
+SUPPORTED = [
+    "md5", "sha1", "sha224", "sha256", "sha384", "sha512"
+]
 
-def new(algorithm, raw=b"") -> Union[
+def new(algorithm: str, raw=b"") -> Union[
         MD5, SHA1, SHA224, SHA256, SHA384, SHA512
     ]:
-    obj = {
-        "md5"    :  MD5,
-        "sha1"   :  SHA1,
-        "sha224" :  SHA224,
-        "sha256" :  SHA256,
-        "sha384" :  SHA384,
-        "sha512" :  SHA512
-    }[algorithm](raw)
+    """ Return Hash Object corresponding to specified algorithm. 
+        Supported algorithm:
+            "md5", "sha1", "sha224", "sha256", "sha384", "sha512"
+    """
+
+    algorithm = algorithm.lower()
+
+    try:
+        if algorithm not in SUPPORTED:
+            raise ValueError(f"[ERROR] Not support \"{algorithm}\" algorithm!")
+        
+        obj = {
+            "md5"    :  MD5,
+            "sha1"   :  SHA1,
+            "sha224" :  SHA224,
+            "sha256" :  SHA256,
+            "sha384" :  SHA384,
+            "sha512" :  SHA512
+        }[algorithm](raw)
+
+    except ValueError as err:
+        print(err)
+        exit(1)
 
     return obj
 
