bc46a3ae0de9acb1971a6aaf7d78978c78a893d8
viensea1106 <viensea1106@gmail.com>
2023-08-09 08:59:41 +0700

    test script to compare my implementation with 'hashlib'

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import HashTools
 import hashlib
 
-from typing import Union
 from os import urandom
 from random import randint
 
@@ -9,23 +8,12 @@ algorithms = [
     "sha1", "sha224", "sha256", "sha384", "sha512"
 ]
 
-def new(algorithm: str):
-    obj = {
-        'sha1'    : HashTools.SHA1  ,
-        'sha224'  : HashTools.SHA224,
-        'sha256'  : HashTools.SHA256,
-        'sha384'  : HashTools.SHA384,
-        'sha512'  : HashTools.SHA512
-    }[algorithm]()
-
-    return obj
-
 def test():
     for alg in algorithms:
         msg = urandom(randint(0, 1024))
 
         py_hash = hashlib.new(alg)
-        my_hash = new(alg)
+        my_hash = HashTools.new(alg)
 
         py_hash.update(msg)
         my_hash.update(msg)
@@ -33,7 +21,7 @@ def test():
         test1 = py_hash.hexdigest()
         test2 = my_hash.hexdigest()
         
-        if not test1 == test2:
+        if test1 != test2:
             print(f"Algorithm {alg} failed the validation test!")
             exit()
     
